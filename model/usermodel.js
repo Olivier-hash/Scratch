4e49a2312046af008c1284b1d38d4f2d9bc0b91f
olivier Rukundo <rr.olivier01@gmail.com>
2025-05-23 10:20:00 +0200

    usermodule and usercontroller added successfully

---
+++ model/usermodel.js
@@ -0,0 +1,33 @@
+
+const {DataTypes} = reuire('sequelize')
+
+
+const dbconn = require('../config/db')
+
+
+const Users = dbconn.define('users', {
+    
+    fullName:{
+        type:DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len:[5]
+        }
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate: {
+            isEmail:true
+        }
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+            len:[8]
+        }
+    }
+})
+
+exports.module = Users
