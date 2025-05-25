61ca09926f6a16e697d79d6a88c7e1ac429d7bb7
olivier Rukundo <rr.olivier01@gmail.com>
2025-05-25 23:33:29 +0200

    SendEmail function from utils added

--- controller/userController.js
+++ controller/userController.js
@@ -118,3 +118,69 @@ exports.VerifyEmail = async (req,res)=>{
 }
 
 
+exports.forgotPassword =async (req,res)=>{
+
+    const{Email} = req.body
+
+    try {
+
+        const user = await UserModel.findOne({where:{Email}});
+        if(!user){
+            res.json('user not found');
+        }
+
+        const token = crypto.randomBytes(32).toString('hex');
+
+         user.token = token ;
+         await user.save();
+
+         const message = `${process.env.BASE_URL}/reset/${user.id}/${token}`
+
+         SendEmail(user.Email , 'reset your password' , message)
+
+        res.json({message:'email send to your account to reset password',  token  })
+        
+    } catch (error) {
+        console.log('error occured', error)
+        
+    }
+    
+}
+
+
+
+
+exports.ResetPassword = async (req, res) => {
+    const { id, token } = req.params;
+    const { newPass } = req.body;
+
+    try {
+        // Check if newPass is provided
+        if (!newPass) {
+            return res.status(400).json({ message: 'New password is required' });
+        }
+
+        const user = await UserModel.findByPk(id);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        if (user.token !== token) {
+            return res.status(400).json({ message: 'Invalid token' });
+        }
+
+        const hashpass = await bcrypt.hash(newPass, 10);
+        user.password = hashpass;
+        user.token = null;
+
+        await user.save();
+
+        res.json({ message: 'Password reset successfully' });
+
+    } catch (error) {
+        console.log('error occurred:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
