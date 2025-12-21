98a04281c8baf3944569ff91cc60177923b08981
olivier Rukundo <rr.olivier01@gmail.com>
2025-12-21 22:15:09 +0100

    Clean up unused imports in authentication logic

--- controller/userController.js
+++ controller/userController.js
@@ -12,7 +12,7 @@ exports.CreateUSer = async (req, res) =>{
     const {fullName, email, password} = req.body
     try {
 
-        // check if user email already exist orit
+        // check if user email already exist ori
         const ExistingUser = await userModel.findOne({where:{email}})
 
         const hashpass = await bcrypt.hash(password, 10);
